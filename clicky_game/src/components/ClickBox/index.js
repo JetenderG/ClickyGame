689993e41cd0113cc7a7cf531c01a554a84b1fd3
Jetender <g.jetender@gmail>
2019-03-06 23:10:58 -0500

    Sefsf

--- clicky_game/src/components/ClickBox/index.js
+++ clicky_game/src/components/ClickBox/index.js
@@ -14,6 +14,8 @@ class ClickBox extends React.Component {
                 <div className="img-coontainer">
 
                     <img alt={props.id} src={props.image} ></img>
+
+                    {onclick =}
                 </div>
             </div>
 
